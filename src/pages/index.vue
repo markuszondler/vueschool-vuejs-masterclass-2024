10a0452985a9d80acaa4dec4b6a6c71862f4eca8
Markus Zondler <contact@markuszondler.com>
2024-11-05 16:48:54 +0100

    feat: add tasks functionality with seeding, types, and new routes

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,10 +1,9 @@
-<script setup lang="ts">
-
-</script>
+<script setup lang="ts"></script>
 
 <template>
   <div>
     <h1>Home Page</h1>
     <RouterLink :to="{ name: '/projects/' }">Projects</RouterLink>
+    <RouterLink :to="{ name: '/tasks/' }">Tasks</RouterLink>
   </div>
 </template>

6bf228d0aaa52025d6ef92cc228c37c3f86d5c45
Markus Zondler <contact@markuszondler.com>
2024-11-20 23:02:47 +0100

    feat: refactor DataTable component and update routing for projects and tasks

--- src/pages/tasks/[id].vue
+++ src/pages/tasks/[id].vue
@@ -2,10 +2,9 @@
 import { useRoute } from 'vue-router'
 const route = useRoute()
 </script>
+
 <template>
   <div>
-    <h1>Project {{ route.params?.id }}</h1>
-    <RouterLink to="/">Home</RouterLink>
-    <RouterLink to="/projects">Projects</RouterLink>
+    <h1>Task {{ route.params?.id }}</h1>
   </div>
 </template>
